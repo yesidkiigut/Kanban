69f4f9b412ab5d4f3e0413e570074b8101b93d2c
Yesid Quintero Gutierrez <yesidkiigut@gmail.com>
2023-08-16 14:12:13 -0500

    se termina el proyecto pendiente a corregir el droppanble}

--- kanbans/src/components/kamban/Kamban.jsx
+++ kanbans/src/components/kamban/Kamban.jsx
@@ -1,7 +1,8 @@
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useState } from 'react';
 import mockData from '../../../src/mockData.js';
-import { Card } from "../card/Card.jsx"
+import { Card } from "../card/Card.jsx";
+import "../kamban/kamban.scss";
 
 
 
@@ -12,10 +13,11 @@ export function Kamban() {
         if (!result.destination) return
         const { source, destination } = result
         if (source.droppableId !== destination.droppableId) {
+
             const sourceColIndex = data.findIndex(e => e.id === source.droppableId)
-            const destiancionColIndex = data.findIndex(e => e.id ===destination.droppableId)
+            const destinationColIndex = data.findIndex(e => e.id ===destination.droppableId)
             const sourceCol = data[sourceColIndex]
-            const destinationCol = data[destiancionColIndex]
+            const destinationCol = data[destinationColIndex]
 
             const sourceTask = [...sourceCol.task]
             const destinationTask =[...destinationCol.task]
@@ -27,17 +29,45 @@ export function Kamban() {
             destinationTask.splice(destination.index,0,remove)
 
             data[sourceColIndex].task = sourceTask
-            data[destiancionColIndex].task = destinationTask
+            data[destinationColIndex].task = destinationTask
 
             setData(data)
         }
     }
     return (
         <DragDropContext onDragEnd={onDragEnd}>
-            <div>
+            <div className='kamban'>
                 {data.map((section) =>( 
                   <Droppable key={section.id} droppableId={section.id}>
-
+                        {(provided) =>(
+                            <div {...provided.droppableProps}
+                            className="kamban-section"
+                            ref={provided.innerRef}>
+                                <div className='kamban-section-title'>
+                                    {section.title}                              
+                                </div>
+                                <div className='kamban-section-content'>
+                                    {section.task.map((task,index)=>(
+                                       <Draggable 
+                                        key={task.id}
+                                        draggableId={task.id}
+                                        index={index}>
+                                            {(provided,snapshot)=>(
+                                                <div 
+                                                ref={provided.innerRef}
+                                                {...provided.droppableProps}
+                                                {...provided.dragHandleProps}
+                                                style={
+                                                    {...provided.draggableProps.style,opacity: snapshot.isDragging?"0.5":"1"}
+                                                }>
+                                                    <Card>{task.title}</Card>
+                                                </div>
+                                            )}
+                                       </Draggable> 
+                                    ))}
+                                </div>
+                            </div>
+                        )}
                   </Droppable>  
                 ))}
             </div>
